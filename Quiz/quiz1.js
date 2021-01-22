9a6052e2d751f4bb38c86d592c010ad6f58bc9b5
Monique Wienholds <snuffelien@gmail.com>
2021-01-22 13:52:21 +0100

    opdrachten toegevoegd

---
+++ Quiz/quiz1.js
@@ -0,0 +1,152 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+		"title": "Games",
+		"imageURI": "https://img.favpng.com/19/19/25/video-games-video-game-consoles-game-controllers-drawing-playstation-png-favpng-p9SCYJsMmic4vTpjmYCL5uVSQ.jpg"
+    },
+    "quizContent":		
+		[
+			{
+				"question": "1. Om welke legendarische Pokémon draait Pokémon Diamond?",
+				"answers": [{
+						"answer": "Girantina",
+						"feedback": false
+					},
+					{
+						"answer": "Dialga",
+						"feedback": true
+					},
+					{
+						"answer": "Arceus",
+						"feedback": false
+					},
+					{
+						"answer": "Palkia",
+						"feedback": false
+					},
+				]
+			},
+			{
+				"question": "2. In welke periode speelt Black Ops 1 zich af?",
+				"answers": [{
+						"answer": "Een verzonnen periode in de toekomst.",
+						"feedback": false
+					},
+					{
+						"answer": "De eerste wereldoorlog.",
+						"feedback": false
+					},
+					{
+						"answer": "De tweede wereld oorlog.",
+						"feedback": false
+					},
+					{
+						"answer": "De koude oorlog.",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "3. Wie is de schurk in Sonic?",
+				"answers": [{
+						"answer": "Shadow",
+						"feedback": false
+					},
+					{
+						"answer": "Dr Eggman",
+						"feedback": true
+					},
+					{
+						"answer": "Knuckles",
+						"feedback": false
+					},
+					{
+						"answer": "Metal Sonic",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "4. Welke game is geen PS3 exclusive?",
+				"answers": [{
+						"answer": "Uncharted",
+						"feedback": false
+					},
+					{
+						"answer": "Killzone",
+						"feedback": false
+					},
+					{
+						"answer": "God of War",
+						"feedback": false
+					},
+					{
+						"answer": "Alice: Madness Returns",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "5. Uit welk jaar komt Halo: Combat Evolved?",
+				"answers": [{
+						"answer": "2001",
+						"feedback": false
+					},
+					{
+						"answer": "2003",
+						"feedback": false
+					},
+					{
+						"answer": "2011",
+						"feedback": false
+					},
+					{
+						"answer": "2000",
+						"feedback": true
+					}
+				]
+			},
+			{
+				"question": "6. Wat was de eerste First Person Shooter?",
+				"answers": [{
+						"answer": "Doom",
+						"feedback": false
+					},
+					{
+						"answer": "Wolfenstein",
+						"feedback": true
+					},
+					{
+						"answer": "Perfect Dark",
+						"feedback": false
+					},
+					{
+						"answer": "Quake",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "7. Welke van deze 4 is het meest verkocht?",
+				"answers": [{
+						"answer": "Super Mario 3D Land",
+						"feedback": false
+					},
+					{
+						"answer": "Fifa 12",
+						"feedback": false
+					},
+					{
+						"answer": "Call of Duty Modern Warfare 3",
+						"feedback": true
+					},
+					{
+						"answer": "Battlefield 3",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
